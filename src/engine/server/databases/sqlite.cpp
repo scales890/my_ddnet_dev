9674b3471aa4210a7135a38bc20fb0edd7bb81ea
scales890 <132640992+scales890@users.noreply.github.com>
2025-04-05 00:52:56 +0800

    修復錯誤

--- src/engine/server/databases/sqlite.cpp
+++ src/engine/server/databases/sqlite.cpp
@@ -54,7 +54,7 @@ public:
 	bool AddPoints(const char *pPlayer, int Points, char *pError, int ErrorSize) override;
 
 	// Here! add
-	virtual bool AddRPoints(const char *pPlayer, int Points, char *pError, int ErrorSize) override;
+	bool AddRPoints(const char *pPlayer, int Points, char *pError, int ErrorSize) override;
 
 	// fail safe
 	bool CreateFailsafeTables();
